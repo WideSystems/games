c40ec070b4887e6433aaee2b44c47b502daa46c9
WideSystems <contatowidesystems@gmail.com>
2015-03-10 15:49:36 -0300

    Primeiro commit :D

---
+++ Fabrica de Doces/Assets/Scripts/Item.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+	public GameObject esquerdaItem;
+	public GameObject direitaItem;
+	public GameObject tinta;
+
+	public float forca;
+	public float torque;
+	private bool isDead;
+	private Vector3 screen;
+
+	public float alpha;
+	public float red;
+	public float green;
+	public float blue;
+
+	private float minY;
+	private float maxY;
+
+	private float rotDirecao = 50;
+
+	// Use this for initialization
+	void Start () {
+		minY = GerenciarCamera.MinY ();
+		maxY = GerenciarCamera.MaxY ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Remover ();
+	}
+
+	public void Remover(){
+		screen = Camera.main.WorldToScreenPoint(transform.position);
+		if(isDead && screen.y < minY){
+			Destroy(gameObject);
+		}else {
+			isDead = true;
+		}
+	}
+
+	public void InstanciarDestruir(){
+		GameObject tempItem = null;
+		GameObject tempTinta = null;
+
+		tempItem = Instantiate (esquerdaItem, transform.position, transform.rotation) as GameObject;
+		tempItem.GetComponent<Rigidbody2D> ().AddForce (-transform.right * forca);
+		Debug.Log (transform.right);
+		//tempItem.AddComponent<Rigidbody2D> ().AddTorque (torque);
+
+		tempItem = Instantiate (esquerdaItem, transform.position, transform.rotation) as GameObject;
+		tempItem.GetComponent<Rigidbody2D> ().AddForce (transform.right * forca);
+		//tempItem.AddComponent<Rigidbody2D> ().AddTorque (torque);
+
+		tempTinta = Instantiate (tinta, new Vector2(transform.position.x, transform.position.y), transform.rotation) as GameObject;
+		//tempTinta.GetComponent<Tinta> ().SetColor (red, green, blue, alpha);
+
+		Destroy (gameObject);
+	}
+}
---
+++ Fabrica de Doces/Assets/Scripts/Prato.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Prato : MonoBehaviour {
+
+	public Vector2 startPos;
+	public Vector2 direction;
+	public bool directionChosen;
+	public string teste = "";
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Update() {
+		// Track a single touch as a direction control.
+		if (Input.touchCount > 0) {
+			var touch = Input.GetTouch(0);
+			
+			// Handle finger movements based on touch phase.
+			switch (touch.phase) {
+				// Record initial touch position.
+			case TouchPhase.Began:
+				startPos = touch.position;
+				directionChosen = false;
+				break;
+				
+				// Determine direction by comparing the current touch position with the initial one.
+			case TouchPhase.Moved:
+				direction = touch.position - startPos;
+				break;
+				
+				// Report that a direction has been chosen when the finger is lifted.
+			case TouchPhase.Ended:
+				directionChosen = true;
+				break;
+			}
+
+			if(direction.x > 0f && transform.position.x == 0f){
+				teste = "MAIOR " + transform.position.x;
+				transform.position = new Vector2(1.8f, transform.position.y);
+			}
+			
+			if(direction.x < 0f && transform.position.x == 0f){
+				teste = "MENOR " + transform.position.x;
+				transform.position = new Vector2(-1.8f, transform.position.y);
+			}
+		}
+
+	}
+
+	void OnGUI(){
+		GUI.contentColor = Color.black;
+		GUI.Label(new Rect (10,0,100,50),"Inicio " + startPos, "color");
+		GUI.Label(new Rect (10,30,100,50),"Movido " + direction, "color");
+		GUI.Label(new Rect (10,60,100,50),"Movido " + teste, "color");
+
+		float merda = GerenciarCamera.MaxX();
+
+		GUI.Label(new Rect (10,80,100,50),"MaxX " +  merda, "color");
+		GUI.Label(new Rect (10,100,100,50),"MinX " + GerenciarCamera.MinX(), "color");
+
+	}
+}
